3e9f6c1153dcc3f78d6884b6d0222f8fe353d521
ɐʇɯon3ɹ <contact.atmoner@gmail.com>
2021-06-10 20:31:33 +0000

    Create config-bitcanna.js

---
+++ config-example/config-bitcanna.js
@@ -0,0 +1,12 @@
+{
+	"mnemonic":"**** *** ** ***** ***** *** ",
+	"validatorAddress":"bcnavaloper******************",
+    "chainId":"bitcanna-testnet-2", 
+    "lcdUrl":"https://cosmos-testnet.bitcanna.io",
+    "denom":"ubcna",
+    "prefix":"bcna",
+    "feeAmount":5000,
+    "AmountSend":1000000,
+    "gasLimit":200000,
+    "memo":"Send from Bcna-Tool"
+}
